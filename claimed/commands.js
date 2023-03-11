cfc88a402f6b5436ba23fed88dc05e404429d2c2
simswipe <65012329+simswipe@users.noreply.github.com>
2023-03-11 23:17:22 +0200

    Add files via upload

---
+++ claimed/commands.js
@@ -0,0 +1,49 @@
+var instagram = 'https://www.instagram.com/k9rean/';
+var tiktok = 'https://www.tiktok.com/@sapanese';
+var discord = 'Claimed#0001';
+var server = 'https://discord.gg/ty';
+
+whois = [
+  "<br>",
+  'Who is <span class="command">Claimed</span>?',
+  "Claimed is one of the few members that are in the killa.lol group.",
+  "you can contact me or find me by using <span class=\"command\">'social'</span> ",
+  "<br>"
+];
+
+social = [
+  "<br>",
+   '<span class="command">Discord</span>         <a href="' + discord + '" target="_blank">@Claimed<span class="command">#</span>0001' + "</a>",
+   '<span class="command">TikTok</span>          <a href="' + tiktok + '" target="_blank"><span class="command">@</span>sapanese' + "</a>",
+   '<span class="command">Instagram</span>       <a href="' + instagram + '" target="_blank"><span class="command">@</span>k9rean' + "</a>",
+   '<span class="command">Server</span>          <a href="' + server + '" target="_blank"><span class="command">@</span>ty' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Displays who claimed? is.',
+  '<span class="command">social</span>         Displays social connections',
+  '<span class="command">aboutme</span>        Displays social connections',
+  '<span class="command">clear</span>          Clears terminal',
+  "<br>",
+];
+
+aboutme = [
+'<div class="flex justify-center items-center space-x-2"><a href="https://discord.com/users/1052623145229635735" target="_blank"><img src="https://discord.c99.nl/widget/theme-4/1052623145229635735.png" style="width: 300px;"></a>',
+'<span class="command">Status</span> | #<span class="command">Block</span>',
+
+
+]
+banner = [
+ " ",
+ "    ██████╗██╗      █████╗ ██╗███╗   ███╗███████╗██████╗ ",
+ "   ██╔════╝██║     ██╔══██╗██║████╗ ████║██╔════╝██╔══██╗",
+ "   ██║     ██║     ███████║██║██╔████╔██║█████╗  ██║  ██║",
+ "   ██║     ██║     ██╔══██║██║██║╚██╔╝██║██╔══╝  ██║  ██║",
+ "   ╚██████╗███████╗██║  ██║██║██║ ╚═╝ ██║███████╗██████╔╝",
+ "    ╚═════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝     ╚═╝╚══════╝╚═════╝ ",
+
+
+  "<span class=\"color2\">for more information about me use </span> <span class=\"command\">'help'</span><span class=\"color2\"></span>",
+];
